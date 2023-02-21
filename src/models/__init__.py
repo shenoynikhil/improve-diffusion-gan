8b72771df7c42e280862007681484949d6d3518a
Nikhil Shenoy <shenoy@login02.sockeye.arc.ubc.ca>
2023-02-20 19:58:48 -0800

    added support for vanilla gan

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,17 +3,23 @@ from src.models.gan_utils import (
     FID,
     ConditionalDiscriminator,
     ConditionalGenerator,
+    Discriminator,
+    Generator,
     SaveGeneratedImages,
     generate_and_save_images,
 )
+from src.models.vanilla_gan import VanillaGAN
 from src.models.wgan import WGAN_GP
 
 __all__ = [
     "FID",
+    "Discriminator",
+    "Generator",
     "ConditionalGenerator",
     "ConditionalDiscriminator",
     "SaveGeneratedImages",
     "generate_and_save_images",
     "WGAN_GP",
     "ACGAN",
+    "VanillaGAN",
 ]
