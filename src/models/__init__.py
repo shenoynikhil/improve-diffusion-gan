76c84a9a2ce217fd42e0743453929e0f3e6a07f6
goulustis <goulusti@student.ubc.ca>
2023-02-25 21:57:39 -0800

    import noise_wgan

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,7 +1,7 @@
 from src.models.acgan import ACGAN, ConditionalDiscriminator, ConditionalGenerator
 from src.models.vanilla_gan import Discriminator, Generator, VanillaGAN
 from src.models.wgan import WGAN_GP
-
+from src.models.wgan_noise import WGAN_NOISE
 __all__ = [
     "Discriminator",
     "Generator",
@@ -10,4 +10,5 @@ __all__ = [
     "WGAN_GP",
     "ACGAN",
     "VanillaGAN",
+    "WGAN_NOISE"
 ]
