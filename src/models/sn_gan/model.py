10d2f575324ceda365ee6f6ba7e772159e5fb3dc
Nikhil Shenoy <shenoy@login03.sockeye.arc.ubc.ca>
2023-03-21 22:27:14 -0700

    Updated sn_gan with corrections

--- src/models/sn_gan/model.py
+++ src/models/sn_gan/model.py
@@ -8,6 +8,7 @@ import torch.nn.functional as F
 from src.models.diffusion import Diffusion
 from src.models.vanilla_gan import VanillaGAN
 
+from ..utils import compute_metrics_no_aux
 from .dg import Discriminator, Generator
 
 
@@ -69,3 +70,61 @@ class SpectralNormGAN(VanillaGAN):
             real_loss = F.binary_cross_entropy_with_logits(real_pred, real_labels)
             fake_loss = F.binary_cross_entropy_with_logits(fake_pred, fake_labels)
             return real_loss + fake_loss
+
+    def training_step(self, batch, batch_idx, optimizer_idx):
+        imgs, _ = batch
+        batch_size = imgs.size(0)
+
+        # sets to same device as imgs
+        valid = torch.ones(batch_size, 1).type_as(imgs)
+        fake = torch.zeros(batch_size, 1).type_as(imgs)
+
+        # generate noise
+        z = torch.normal(0, 1, (batch_size, self.latent_dim, 1, 1)).type_as(imgs)
+
+        # Generate a batch of images
+        gen_imgs = self.generator(z)
+
+        # construct step output
+        step_output = {"gen_imgs": gen_imgs}
+
+        # train generator
+        if optimizer_idx == 0:
+            # Loss measures generator's ability to fool the discriminator
+            validity = self.discriminator(gen_imgs)
+            g_loss = self.generator_loss(validity, valid)
+
+            # log generator loss
+            self.log("g_loss", g_loss, prog_bar=True)
+
+            # update step_output
+            step_output["loss"] = g_loss
+
+            return step_output
+
+        # train discriminator
+        if optimizer_idx == 1:
+            # Loss for real images
+            real_pred = self.discriminator(imgs)
+            fake_pred = self.discriminator(gen_imgs)
+
+            # compute discriminator loss
+            d_loss = self.discriminator_loss(real_pred, fake_pred, valid, fake)
+
+            # log discriminator loss
+            self.log("d_loss", d_loss, prog_bar=True)
+
+            # compute metrics
+            metrics = compute_metrics_no_aux(
+                real_pred,
+                fake_pred,
+                valid,
+                fake,
+            )
+
+            self.log_dict(metrics, prog_bar=True)
+
+            # update step_output
+            step_output["loss"] = d_loss
+
+            return step_output
