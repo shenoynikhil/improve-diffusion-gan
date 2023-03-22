afa822327030d5db426d0c87c37bf7d379f1dec9
Nikhil Shenoy <shenoy@login03.sockeye.arc.ubc.ca>
2023-03-21 19:28:35 -0700

    removed diffusion_gan and wgan to reduce complexity

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,9 +1,7 @@
 from src.models.acgan import ACGAN, ConditionalDiscriminator, ConditionalGenerator
-from src.models.diffusion_gan import DiffusionGAN
-from src.models.diffusion_wgan import DiffusionWGAN
 from src.models.sn_gan import SpectralNormGAN
 from src.models.vanilla_gan import Discriminator, Generator, VanillaGAN
-from src.models.wgan import WGAN_GP
+from src.models.wgan_gp import WGAN_GP
 from src.models.wgan_noise import WGAN_NOISE
 
 __all__ = [
@@ -12,8 +10,6 @@ __all__ = [
     "ConditionalGenerator",
     "ConditionalDiscriminator",
     "WGAN_GP",
-    "DiffusionGAN",
-    "DiffusionWGAN",
     "ACGAN",
     "SpectralNormGAN",
     "VanillaGAN",
