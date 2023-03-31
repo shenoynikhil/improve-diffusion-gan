61df9a60633456efef782d7b7044a14458cd88e1
goulustis <goulusti@student.ubc.ca>
2023-03-31 16:07:20 -0700

    import ndb score on callbacks __init__

--- src/callbacks/__init__.py
+++ src/callbacks/__init__.py
@@ -1,9 +1,10 @@
 from .fid import FID
 from .inception_score import IS
 from .save_generated_images import SaveGeneratedImages
-
+from .ndb import NDB_Score
 __all__ = [
     "IS",
     "FID",
     "SaveGeneratedImages",
+    "NDB_Score"
 ]
